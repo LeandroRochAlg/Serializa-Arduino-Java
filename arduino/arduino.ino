26f9e7f1cbf560cb96d0b8abaf2ad6ae6a9a91aa
LeandroRochAlg <leandrochaju@hotmail.com>
2023-11-07 13:23:54 -0300

    Comentários e melhorias

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,8 @@
-byte msg[16];
-int num;
+byte msg[16]; // Vetor de bytes que armazena a mensagem recebida
+int num;      // Variável que armazena o número montado
+int fim = 24; // Número que indica o fim da mensagem
 
+// Função que recebe um vetor de bytes e uma posição e retorna um inteiro montado por 2 bytes
 int deserializar(byte vet[], int pos){
   num = msg[pos];
   num = num << 8;
@@ -9,36 +11,36 @@ int deserializar(byte vet[], int pos){
   return num;
 }
 
+// Configuração inicial do Arduino
 void setup() {
   Serial.begin(9600);
-  Serial.println("Leandro");
 }
 
+// Função principal do Arduino
 void loop() {
-  if(Serial.available()){
-    Serial.readBytes(msg,16);
+  if(Serial.available()){ // Se houver dados disponíveis na porta serial
+    Serial.readBytes(msg,16); // Lê os dados disponíveis e armazena no vetor msg
 
-    int vet[8];
-    int soma = 0, i;
+    int vet[8];       // Vetor que armazena os números da mensagem
+    int soma = 0, i;  // Variáveis auxiliares
 
-    for(i = 0; i<8; i++){
+    for(i = 0; i<8; i++){ // Percorre o vetor de bytes e monta o vetor de inteiros
       vet[i] = deserializar(msg, i*2);
 
-      if(i>2){
-        if(vet[i-1]==24) {
-				  break;
-        }
+      if(vet[i] == fim){ // Se encontrar o fim da mensagem, monta a soma e sai do loop
+        vet[i+1] = deserializar(msg, i*2+2);
+        break;
       }
     }
 
-    for(int j = 1; j<i-1; j++){
+    for(int j = 1; j<i-1; j++){ // Percorre o vetor de inteiros e calcula a soma
       soma += vet[j];
     }
 
-    if(soma == vet[i]){
-      Serial.print("1");
+    if(soma == vet[i]){ // Verifica se a soma está correta
+      Serial.print("Soma correta!!!");
     }else{
-      Serial.print("0");
+      Serial.print("Soma incorreta :(");
     }
   }
 }
--- envio/src/Main.java
+++ envio/src/Main.java
@@ -4,38 +4,39 @@ import jssc.SerialPortException;
 public class Main {
     public static void main(String[] args) {
 
-        byte vet[] = new byte[16];
-        int[] nums = {42, 1, 2, 3, 4, 5, 24, 15};
+        byte vet[] = new byte[16];                      // Vetor de bytes que será enviado
+        int fim = 24;                                   // Número que indica o fim da sequência
+        int inicio = 42;                                // Número que indica o início da sequência
+        int[] nums = {inicio, 1, 2, 3, 4, 5, fim, 15};  // Números que serão montados e enviados
 
         for(int i = 0; i<8; i++) {
             serializar(vet, nums[i], i*2);
 
-            if(i>2) {
-                if(nums[i-1]==24) {
-                    break;
-                }
+            if(nums[i] == fim){
+                serializar(vet, nums[i+1], i*2+2);
             }
         }
 
-        SerialPort porta = new SerialPort("COM6");
+        SerialPort porta = new SerialPort("COM6");    // Porta serial que será utilizada
 
-        try {
+        try {   // Tenta abrir a porta serial
             porta.openPort();
-            porta.setParams(9600, 0, 1, 0, false, false);
-            porta.writeBytes(vet);
+            porta.setParams(9600, 8, 1, 0, false, false);
+            porta.writeBytes(vet);  // Envia o vetor de bytes
             porta.closePort();
         } catch (SerialPortException e) {
-
             e.printStackTrace();
         }
     }
 
-    public static void serializar(byte vet[], int num, int pos) {
-        byte b1, b2;
+    // Método que serializa um inteiro em um vetor de bytes
+    public static void serializar(byte[] vet, int num, int pos) {
+        byte b1, b2;    // Bytes que serão utilizados para serializar o inteiro
 
-        b2 = (byte)(num & 0xFF);
-        b1 = (byte)( (num >> 8) & 0xFF );
+        b2 = (byte)(num & 0xFF);        // Pega os 8 primeiros bits do inteiro
+        b1 = (byte)((num >> 8) & 0xFF); // Pega os 8 últimos bits do inteiro
 
+        // Coloca os bytes no vetor de bytes
         vet[pos] = b1;
         vet[pos+1] = b2;
     }
